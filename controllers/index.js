09de943b3d315341c0cb53346b61e6bacbd51e75
PoghosKeshishyan <poghos877@gmail.com>
2025-12-23 01:06:32 +0400

    first commit

--- controllers/index.js
+++ controllers/index.js
@@ -3,13 +3,13 @@ const getHomePage = async (req, res) => {
     const projectVersion = 'v.1.8.'
 
     const routes = [
-        { id: 1, method: 'GET', access: 'public', url: `${req.baseFullUrl}/api/header_infos` },
-        { id: 2, method: 'GET', access: 'public', url: `${req.baseFullUrl}/api/header_infos?lang=EN` },
-        { id: 3, method: 'GET', access: 'public', url: `${req.baseFullUrl}/api/header_infos/53412227-86ba-4aa3-9a48-1f6884986eac` },
-        { id: 4, method: 'GET', access: 'public', url: `${req.baseFullUrl}/api/services_pages/services-data/8e4fadc3-ebc0-47be-8ecb-e72ad66a6dcb` },
-        { id: 5, method: 'POST', access: 'private', url: `${req.baseFullUrl}/api/header_infos` },
-        { id: 6, method: 'PUT', access: 'private', url: `${req.baseFullUrl}/api/header_infos/53412227-86ba-4aa3-9a48-1f6884986eac` },
-        { id: 7, method: 'DELETE', access: 'private', url: `${req.baseFullUrl}/api/header_infos/53412227-86ba-4aa3-9a48-1f6884986eac` },
+        { id: 1, method: 'GET', access: 'public', url: `${req.baseFullUrl}/api/header-infos` },
+        { id: 2, method: 'GET', access: 'public', url: `${req.baseFullUrl}/api/header-infos?lang=EN` },
+        { id: 3, method: 'GET', access: 'public', url: `${req.baseFullUrl}/api/header-infos/53412227-86ba-4aa3-9a48-1f6884986eac` },
+        { id: 4, method: 'GET', access: 'public', url: `${req.baseFullUrl}/api/services-pages/services-data/8e4fadc3-ebc0-47be-8ecb-e72ad66a6dcb` },
+        { id: 5, method: 'POST', access: 'private', url: `${req.baseFullUrl}/api/header-infos` },
+        { id: 6, method: 'PUT', access: 'private', url: `${req.baseFullUrl}/api/header-infos/53412227-86ba-4aa3-9a48-1f6884986eac` },
+        { id: 7, method: 'DELETE', access: 'private', url: `${req.baseFullUrl}/api/header-infos/53412227-86ba-4aa3-9a48-1f6884986eac` },
     ];
 
     const resources = [
